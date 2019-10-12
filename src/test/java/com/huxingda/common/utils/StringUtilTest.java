fb2ba59840df15f372da47ad57212d05f6281590
lenovo <lenovo@LAPTOP-2MLS2N3S>
2019-10-12 10:34:15 +0800

    说明

---
+++ src/test/java/com/huxingda/common/utils/StringUtilTest.java
@@ -0,0 +1,62 @@
+package com.huxingda.common.utils;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void testHasLength() {
+		String str="";
+		System.out.println(StringUtil.hasLength(str));
+	}
+
+	@Test
+	public void testHasText() {
+		String str="abc";
+		System.out.println(StringUtil.hasText(str));
+	}
+
+	@Test
+	public void testRandomChineseString() {
+		String string=StringUtil.randomChineseString();
+		System.out.println(string);
+	}
+
+	@Test
+	public void testRandomChineseStringInt() {
+		String string=StringUtil.randomChineseString(100);
+		System.out.println(string);
+	}
+
+	@Test
+	public void testGenerateChineseName() {
+		for (int i = 0; i <100; i++) {
+			String name=StringUtil.generateChineseName();
+			System.out.println(name);
+		}
+	}
+	@Test
+	public void testPerson() {
+		for (int i = 0; i < 10000; i++) {
+			//姓名
+			String name=StringUtil.generateChineseName();
+			//年龄
+			int age = RandomUtil.random(1,120);
+			//介绍
+			String abount=StringUtil.randomChineseString(140);
+			//注册日期
+			Calendar c = Calendar.getInstance();
+			c.set(2010,0,1);
+			
+			Date created = (Date) DateUtil.getDate(c.getTime(), new Date());
+			Person p = new Person(name, age, abount, created);
+			System.out.println(p);
+		}
+	}
+
+}
